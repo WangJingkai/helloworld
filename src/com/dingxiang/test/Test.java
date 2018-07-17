4715c064026570b0bb34849e9bd7f88af2c58023
DELL <DELL@DESKTOP-POANLDB>
2018-07-17 17:31:40 +0800

    test

--- src/com/dingxiang/test/Test.java
+++ src/com/dingxiang/test/Test.java
@@ -2,6 +2,6 @@ package com.dingxiang.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world,ÊÀ½ç£¬ÄãºÃ");
 	}
 }
